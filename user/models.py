47948957e36d346ae81b87ab7e75630543a11f6a
Ruslan Priadko <r.priadko@ukr.net>
2025-09-15 14:16:04 +0300

    Deleted unnecessary models

--- user/models.py
+++ user/models.py
@@ -1,52 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
     pass
-
-class Actor(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-
-class Genre(models.Model):
-    name = models.CharField(max_length=100,
-                            unique=True)
-
-
-class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    duration = models.IntegerField()
-    actors = models.ManyToManyField(
-        Actor,
-        related_name="movies"
-    )
-    genres = models.ManyToManyField(
-        Genre,
-        related_name="movies"
-    )
-
-    def __str__(self):
-        return self.title
-
-
-class CinemaHall(models.Model):
-    name = models.CharField(max_length=100)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
-
-
-class MovieSession(models.Model):
-    show_time = models.DateTimeField()
-    movie = models.ForeignKey(
-        Movie,
-        on_delete=models.CASCADE,
-        related_name="movie_sessions"
-    )
-    cinema_hall = models.ForeignKey(
-        CinemaHall,
-        on_delete=models.CASCADE,
-        related_name="movie_sessions"
-    )
